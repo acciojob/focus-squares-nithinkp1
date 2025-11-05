f6342ab98817d3223147bb83efb72c31cf13131a
nithinkp1 <131663803+nithinkp1@users.noreply.github.com>
2025-11-05 19:22:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll('.square');
+
+    squares.forEach((square, index) => {
+      square.addEventListener('mouseenter', () => {
+        squares.forEach((s, i) => {
+          if (i !== index) s.style.backgroundColor = '#6F4E37'; // Coffee
+        });
+      });
+
+      square.addEventListener('mouseleave', () => {
+        squares.forEach(s => s.style.backgroundColor = '#E6E6FA'); // Lavender
+      });
+    });
